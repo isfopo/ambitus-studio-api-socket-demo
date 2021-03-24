bf8dc86bd42ed9528c2db6158cf43dcb2b54eb8e
Isaac Poole <isaacpoolemusic@gmail.com>
2021-03-24 15:36:00 -0400

    adds demo code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+
+import { Manager } from "socket.io-client";
+import "./App.css";
+const ENDPOINT = "http://localhost:3000";
 
 function App() {
+  const socket = useRef();
+
+  const [ProjectId, setProjectId] = useState("");
+  const [token, setToken] = useState("");
+  const [project, setProject] = useState("");
+  const [error, setError] = useState("");
+
+  const connect = () => {
+    const manager = new Manager(ENDPOINT, {
+      reconnectionDelayMax: 10000,
+      query: {
+        ProjectId,
+      },
+    });
+
+    socket.current = manager.socket("/", {
+      auth: {
+        token: `Bearer ${token}`,
+      },
+    });
+
+    socket.current.on("project", (data) => {
+      setProject(data.project);
+      console.log(data);
+    });
+
+    socket.current.on("update", (data) => {
+      console.log(data);
+    });
+
+    socket.current.on("error", (error) => {
+      setError(error);
+      console.log(error);
+    });
+  };
+
+  const disconnect = () => {
+    socket.current.disconnect();
+    setProject("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <label>
+          ProjectId:
+          <input
+            type="text"
+            value={ProjectId}
+            onChange={(e) => setProjectId(e.target.value)}
+          />
+        </label>
+      </form>
+      <br />
+      <form>
+        <label>
+          Token:
+          <input
+            type="text"
+            value={token}
+            onChange={(e) => setToken(e.target.value)}
+          />
+        </label>
+      </form>
+      <br />
+      <button onClick={connect}>Connect</button>
+      <button onClick={disconnect}>Disconnect</button>
+      <br />
+      {error ? (
+        <h1>{error}</h1>
+      ) : (
+        <p>{project ? `Connected to ${project.name}` : `Not connected`}</p>
+      )}
     </div>
   );
 }
